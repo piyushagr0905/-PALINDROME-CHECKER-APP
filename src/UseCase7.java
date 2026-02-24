b008f157dda0736040f045169f07fad5b26805e8
piyushagr0905 <agarwallapiyush27@gmail.com>
2026-02-24 14:43:27 +0530

    UC7:

---
+++ src/UseCase7.java
@@ -0,0 +1,44 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String input) {
+        input = input.toLowerCase().replaceAll("\\s+", "");
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== Palindrome Checker App (UC7 - Deque Based) =====");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
